487568034d1e8ffeb63a737882b0cae9825e29e7
chukwuemekaumesi <chukwuemekaumesi@gmail.com>
2023-09-09 11:08:08 -0700

    Fixed

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,10 +10,12 @@ int main(void)
 {
 	char lowercase;
 
-	for (lowercase = 'z'; lowercase >= 'a'; lowercase++)
+	for (lowercase = 'z'; lowercase >= 'a'; lowercase--)
 	{
 		putchar(lowercase);
 	}
+
+	putchar('\n');
 
 	return (0);
 }
