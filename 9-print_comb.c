5cf335bfeedb88460abcac25ad2deb172426183b
chukwuemekaumesi <chukwuemekaumesi@gmail.com>
2023-09-10 10:45:18 -0700

    Fixed

--- 9-print_comb.c
+++ 9-print_comb.c
@@ -14,12 +14,10 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 9; j++)
-		{
-			putchar(i + '0');
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+		
 	}
 	putchar('\n');
 	return (0);
