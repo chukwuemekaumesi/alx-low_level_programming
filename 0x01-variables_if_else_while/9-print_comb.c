00e440bd905bb890945f04181d4830f925469e91
chukwuemekaumesi <chukwuemekaumesi@gmail.com>
2023-09-10 10:58:57 -0700

    Corrected

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,19 +10,12 @@ int main(void)
 {
 	int i;
 
-	int j;
-
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
-		{
-			if (i < j)
-			{
-				putchar(i + '0');
-				putchar(',');
-				putchar(' ');
-			}
-		}
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	}
 	putchar('\n');
 	}
 	return (0);
