7cdbc28515f75940f3d34b1ae5c0d0cba7c5efa8
chukwuemekaumesi <chukwuemekaumesi@gmail.com>
2023-09-10 18:35:09 -0700

    project and combination of numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,4 +8,26 @@
 
 int main(void)
 {
+	int i;
+
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+
+			if (i != 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}	
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
 }
