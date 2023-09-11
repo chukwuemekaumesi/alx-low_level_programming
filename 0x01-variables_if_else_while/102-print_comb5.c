21206d5d987f32dde9c8a140de81dcd0f0998fcf
chukwuemekaumesi <chukwuemekaumesi@gmail.com>
2023-09-11 05:18:40 -0700

    Fixing things

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 
 int main(void)
 {
@@ -11,25 +13,24 @@ int main(void)
 
 	int j;
 
-	for (i = 0; i < 100; i++)
+	for (i = 0; i < 100; i++);
 	{
-		for (j = i + 1; j < 100; j++);
+		for (j =0; j < 99; j++);
 		{
 			putchar(i / 10 + '0');
 			putchar(i % 10 + '0');
 			putchar(' ');
 			putchar(j / 10 + '0');
 			putchar(j % 10 + '0');
-
-			if (i != 99 && j != 98)
+			if (i != 98 || j != 97)
 			{
 				putchar(',');
 				putchar(' ');
+			
 			}
+		
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
-
