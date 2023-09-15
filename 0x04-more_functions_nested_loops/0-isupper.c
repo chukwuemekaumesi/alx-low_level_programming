8901c7cb0ad7e249a9afd22b77eaa5fe18735d4d
chukwuemekaumesi <chukwuemekaumesi@gmail.com>
2023-09-15 06:02:33 -0700

    fixing

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,20 +3,21 @@
 /**
  * _isupper - check if C is in uppercase
  * @c:  character in the program
+ *
  * Return: 1
  */
 
 int _isupper(int c)
 {
-	if (c => 67 && c <= 90)
-	{
+	if (c >= 67 && c <= 90)
+	
 		return (1);
-	}
+	
 
 	else
-	{
+	
 		return (0);
-	}
+	
 
 
 }
