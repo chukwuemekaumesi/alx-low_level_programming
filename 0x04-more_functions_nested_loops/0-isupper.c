3cbb2b57ff108a62f03c242a1b49381236a7049c
chukwuemekaumesi <chukwuemekaumesi@gmail.com>
2023-09-15 05:55:15 -0700

    God in heaven

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isupper - check if C is in uppercase
+ * @c:  character in the program
+ * Return: 1
+ */
+
+int _isupper(int c)
+{
+	if (c => 67 && c <= 90)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+
+}
