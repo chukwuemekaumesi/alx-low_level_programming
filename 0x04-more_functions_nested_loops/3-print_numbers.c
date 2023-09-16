792ad927140b4fdbb2eef5012928fd84e3ff08a5
chukwuemekaumesi <chukwuemekaumesi@gmail.com>
2023-09-15 18:28:17 -0700

    not right

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,16 +4,16 @@
 /**
  * print_numbers - print the integers between 0 and 9
  *
- * Return: return 0 if successful
+ * Return: Void
  */
 
 void print_numbers(void)
 {
-	int i;
+	char i;
 
-	for (i = 0; i < 10; i++)
+	for (i = '0'; i < '10'; i++)
 	{
-		_putchar(i + '0');
+		_putchar(i);
 	}
 	_putchar('\n');
 }
