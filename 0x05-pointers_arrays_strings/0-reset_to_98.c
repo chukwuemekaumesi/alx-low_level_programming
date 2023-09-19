dadea9b44562f92fc618286b79f29623db3fffcc
chukwuemekaumesi <chukwuemekaumesi@gmail.com>
2023-09-18 20:12:23 -0700

    corrected

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,14 +2,11 @@
 
 /**
  * Main - To write pointers 
- * @n: the pointer variable i will be working with
+ * @*n: the pointer variable i will be working with
  * Return: It will return 0 since the function is an integer
  */
 
 void reset_to_98(int *n)
 {
-	int *n;
-
 	*n = 98;
-
 }
