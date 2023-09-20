65db6ccd2f24d54f1053287b97fe5346011b10d2
chukwuemekaumesi <chukwuemekaumesi@gmail.com>
2023-09-19 17:43:56 -0700

    thank god for error messages

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
- * Main - To write pointers 
+ * reset_to_98 - To write pointers 
  * @*n: the pointer variable i will be working with
  * Return: It will return 0 since the function is an integer
  */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
