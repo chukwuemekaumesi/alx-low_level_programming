40f1dc31e6907e10035d3e77be503634fed826e8
chukwuemekaumesi <chukwuemekaumesi@gmail.com>
2023-09-22 09:02:32 -0700

    Good Grace

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -19,20 +19,16 @@ char *_strncat(char *dest, char *src, int n)
 	int j;
 
 	i = 0;
-
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
-
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
 		i++;
 		j++;
 	}
-
 	dest[i] = '\0';
 	return (dest);
-
 }
