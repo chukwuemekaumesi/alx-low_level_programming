f531121565a21a4701c97cb6f4e6d1996fa13090
chukwuemekaumesi <chukwuemekaumesi@gmail.com>
2023-09-22 20:15:37 -0700

    good

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,13 +20,13 @@ char *cap_string(char *str)
 		{
 			str[i] = str[i] - 32;
 		}
-		for (j = 0; c[j] != '\0'; j++)
+	for (j = 0; c[j] != '\0'; j++)
+	{
+		if (c[j] == str[i] && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
 		{
-			if (c[j] == str[i] && str[i + 1] >= 'a' && str[i + 1] <= 'z')
-			{
-				str[i + 1] = str[i + 1] - 32;
-			}
+			str[i + 1] = str[i + 1] - 32;
 		}
+	}
 	}
 	return (str);
 }
