218f0b474396f6f87a72cb4cfba683b9cdaf699e
chukwuemekaumesi <chukwuemekaumesi@gmail.com>
2023-09-22 19:36:37 -0700

    first letter to uppercase

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize all strings
+ * @str: the pointer
+ * Return: the pointer
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+
+	int j;
+
+	char c[] = {44, 59, 46, '!', '?', '"', '(', ')', '{', '}', '\t', '\n', ' '};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 && str[i] == 'a' && str[i] == 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		for (j = 0; c[j] != '\0'; j++)
+		{
+			if (c[j] == str[i] && str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
+		}
+	}
+	return (str);
+}
