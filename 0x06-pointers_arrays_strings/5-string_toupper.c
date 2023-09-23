d5a046d37103504ab7f34cd04fb7c0deff4be194
chukwuemekaumesi <chukwuemekaumesi@gmail.com>
2023-09-22 18:34:59 -0700

    lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase to upper
+ * @*: the pointer takes in character
+ * Return: uppercase
+ */
+
+char *string_toupper(char *)
+{
+	int i;
+
+	i = 0;
+
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] = n[i] - 32;
+		}
+		i++;
+	}
+	return(n);
+
+}
