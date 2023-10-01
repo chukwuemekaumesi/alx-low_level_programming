14a7d43844f01585372f9c2494f2c1a47d93ea20
chukwuemekaumesi <chukwuemekaumesi@gmail.com>
2023-10-01 11:16:30 -0700

    completed palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+/**
+ * strnlen - gives us string length
+ * @s: takes in string
+ * Return: return string length
+ */
+
+int strnlen(char *s)
+{
+	if (*s != '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + strnlen(s + 1));
+	}
+}
+
+
+
+
+/**
+ * compare_str - compare string characters
+ * @s: the string
+ * @a: first compare
+ * @b: second compare
+ * Return: int
+ */
+
+int compare_str(char *s, int a, int b)
+{
+	if (*(s + a) == *(s + b))
+	{
+		if (a == b || a == b + 1)
+		{
+			return (1);
+		}
+		return (0 + compare_str(s, a + 1, b - 1));
+	}
+	return (0);
+}
+
+
+
+/**
+ * is_palindrome - check for palindrome
+ * @s: the string
+ * Return: return 0 or 1
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+	{
+		return (1);
+	}
+	else
+	{
+		return (compare_str(s, 0, strnlen(s - 1)));
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,4 +11,8 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int sqr_a(int a, int b);
 int is_prime_number(int n);
+int is_palindrome(char *s);
+int compare_str(char *s, int a, int b);
+int strnlen(char *s);
+
 #endif
