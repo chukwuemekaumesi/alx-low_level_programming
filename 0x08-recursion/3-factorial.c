fb53b0faf539d2bd3acfbb6f4ea0c98870c51522
chukwuemekaumesi <chukwuemekaumesi@gmail.com>
2023-10-01 07:37:53 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial - multiplication of lower numbers
+ * @n: the integer taken
+ * Return: the factorial of the number
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	else if
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
