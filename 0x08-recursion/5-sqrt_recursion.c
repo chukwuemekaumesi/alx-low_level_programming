e24bd05e82404820c869dd3c65db04599484deba
chukwuemekaumesi <chukwuemekaumesi@gmail.com>
2023-10-01 09:41:18 -0700

    testing

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,32 +1,4 @@
-#include "main.h"
-
-/**
- * square_root - checks if a number has natural
- * suqare root
- * @q: the interator
- * @p: the integer taken
- * Return: the square
- */
-
-int square_root(int p, int q)
-{
-	p = 1;
-
-	if (q * q == p)
-	{
-		return (q);
-	}
-	else if (q < n)
-	{
-		square_root(p, ++q);
-	}
-	else
-	{
-		return (-1);
-	}
-}
-
-
+#include "main.h"i
 
 /**
  * _sqrt_recursion - gives me the square of a number
@@ -38,5 +10,30 @@ int _sqrt_recursion(int n)
 {
 	int square_root(int p, int q);
 
+	int q = 1;
 	return (square_root(n, q));
+}
+
+/**
+ * square_root - checks if a number has natural
+ * suqare root
+ * @q: the interator
+ * @p: the integer taken
+ * Return: the square
+ */
+
+int square_root(int p, int q)
+{
+	if (q * q == p)
+	{
+		return (q);
+	}
+	else if (q < p)
+	{
+		square_root(p, ++q);
+	}
+	else
+	{
+		return (-1);
+	}
 }
