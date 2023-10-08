8ea26ba82bc49817c3808c2f075deffdf55a58a4
chukwuemekaumesi <chukwuemekaumesi@gmail.com>
2023-10-07 19:21:42 -0700

    learning

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -16,14 +16,12 @@ int main(int argc, char *argv[])
 {
 	int cents, mcoin = 0;
 
-	if (argc > 2)
+	if (argc == 1 || argc > 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
 	cents = atoi(argv[1]);
-
 	while (cents > 0)
 	{
 		if (cents >= 25)
