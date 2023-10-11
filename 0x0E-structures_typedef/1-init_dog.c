c30ce2caea0abca52b7d1d2d2cc8507b6706d649
chukwuemekaumesi <chukwuemekaumesi@gmail.com>
2023-10-11 16:57:30 -0700

    fixed without memory allocation

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * init_dog - the function we want to use
@@ -11,13 +12,12 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
 	if (d == NULL)
 	{
-		return (NULL);
+		return;
 	}
-	*(d).name = name;
-	*(d).age = age;
-	*(d).owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
 
