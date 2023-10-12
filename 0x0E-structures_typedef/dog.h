ae2dc641d50d95236c0ee16fa257e6e9f6b8ff28
chukwuemekaumesi <chukwuemekaumesi@gmail.com>
2023-10-11 17:23:46 -0700

    new name for struct dog using typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,16 +9,16 @@
  * Description: the whole dog description
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}; tog_t
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-struct dog dog_t;
+
 
 
 #endif
