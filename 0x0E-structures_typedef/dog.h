79590ce493ea4cd4a24f2a3724430a5d1fb25250
chukwuemekaumesi <chukwuemekaumesi@gmail.com>
2023-10-11 17:30:57 -0700

    betty fixed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,16 +9,12 @@
  * Description: the whole dog description
  */
 
-/**
- * dog_t - typedef for dog
- */
-
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
