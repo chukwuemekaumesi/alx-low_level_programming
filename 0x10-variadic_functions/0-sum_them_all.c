8089839b944b7a50c0ae74a0a32ab395433cc812
chukwuemekaumesi <chukwuemekaumesi@gmail.com>
2023-10-12 17:14:05 -0700

    corrected

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -20,13 +20,13 @@ int sum_them_all(const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		if (n = 0)
+		if (n == 0)
 		{
 			return (0);
 		}
 		else
 		{
-			sum += va_arg(arg, int);
+			sum += va_arg(arg, const unsigned int);
 		}
 	}
 
