30b1bc763ce288b1d96fd0fbac35fccf6c00ceb7
chukwuemekaumesi <chukwuemekaumesi@gmail.com>
2023-10-25 15:41:36 -0700

    print the data passed

---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+The second singly linked list
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct listint_t - self referential struct for
+ * creating lists
+ * @n: takes in the number
+ * @next: pointer to the next node of a list
+ */
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+size_t print_listint(const listint_t *h);
+
+
+#endif
