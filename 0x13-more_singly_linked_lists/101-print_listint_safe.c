021801e03cbbb93be53556a51fe9235deb37b39f
chukwuemekaumesi <chukwuemekaumesi@gmail.com>
2023-10-25 21:24:49 -0700

    again

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe -prints a linked list
+ * @head: the head
+ * Return: return the linked list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	listint_t
+}
